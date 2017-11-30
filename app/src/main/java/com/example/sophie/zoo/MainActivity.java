139f0cd5ca51b3ac5c5586af1edcb778bc906f0f
Sophie Miller <sophie713713@gmail.com>
2017-11-30 01:15:33 +0100

    update 2

--- app/src/main/java/com/example/sophie/zoo/MainActivity.java
+++ app/src/main/java/com/example/sophie/zoo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.sophie.zoo;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //create a list of animals
         ArrayList<Animal> animals = new ArrayList<>();
-        animals.add(new Animal(R.drawable.hedgehog_d, "Hedgehog","Toby"));
-        animals.add(new Animal(R.drawable.cat, "Cat","Mici"));
-        animals.add(new Animal(R.drawable.dog, "Dog","Bone"));
-        animals.add(new Animal(R.drawable.panda, "Panda","Sia"));
-        animals.add(new Animal(R.drawable.bat, "Bat","Posty"));
+        animals.add(new Animal(R.drawable.hedgehog_d, "Hedgehog","Toby", R.raw.hedgehog));
+        animals.add(new Animal(R.drawable.cat, "Cat","Mici", R.raw.cat));
+        animals.add(new Animal(R.drawable.dog, "Dog","Bone", R.raw.dog));
+        animals.add(new Animal(R.drawable.panda, "Panda","Sia", R.raw.panda));
+        animals.add(new Animal(R.drawable.bat, "Bat","Posty", R.raw.bat));
 
 //put animals into the view
         AnimalAdapter animalList = new AnimalAdapter(this, animals);
@@ -27,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
 //set the animal view to the list
         animalListView.setAdapter(animalList);
 
+        animalListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mPlayer = MediaPlayer.create(MainActivity.this, R.raw.bat);
+                mPlayer.start();
+            }
+        });
+
     }
+
 }
